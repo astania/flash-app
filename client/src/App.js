9cf3f2dacdfe16768fb166bd84cc5337972725ce
+ <+>
2022-12-19 16:56:38 -0500

    create login frontend files

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import { useState, useEffect } from "react";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import WelcomePage from "./login components/WelcomePage";
+import WelcomePage from "./login_components/WelcomePage";
+import LoginPage from "./login_components/LoginPage"
+
+// sudo service postgresql start
 
 function App() {
   
-
-
+  const [loggedIn, setLoggedIn] = useState(false)
 
   return (
 
     <BrowserRouter>
       {/* <Header /> */}
       {/* {!!loggedIn ? <NavBar /> : ""} */}
+      
       <Routes>
         {/* <Route exact path="/" element={!!loggedIn ? <WelcomePage user={user} /> : <Login user={user} setUser={setUser} onLogin={onLogin} />} /> */}
-        <Route exact path="/" element={<WelcomePage />} />
+        <Route exact path="/" element={!!loggedIn ? <WelcomePage /> : <LoginPage />} />
         {/* <Route exact path="/login" element={<Login />} /> */}
 
       </Routes>
